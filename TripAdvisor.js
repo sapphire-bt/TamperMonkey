a9abc3fa14349f02ff9bbc5f8f5c7dd67e82ecb6
Sapphire <33162278+sapphire-bt@users.noreply.github.com>
2018-11-08 19:53:14 +0000

    Update TripAdvisor.js

--- TripAdvisor.js
+++ TripAdvisor.js
@@ -7,51 +7,67 @@
 
 (function() {
 
-	// Get all attraction links with onclick attributes.
-	var trashLinks = document.querySelectorAll(".attraction_list [onclick]");
+	// Add mutation observer to main results wrapper.
+	var linkWrapper = document.getElementById("MAIN_PAGE_FRAGMENT");
+	var observer    = new MutationObserver(removeTrash);
 
-	if (trashLinks) {
+	observer.observe(linkWrapper, {
+		attributes : true,
+		childList  : true,
+		subtree    : true
+	})
 
-		console.log("[TripAdvisor TRASH] Removing trash onclick elements.");
+	// Run function.
+	removeTrash();
 
-		for (var i = 0; i < trashLinks.length; i++) {
-			var el = trashLinks[i];
+	// Main function.
+	function removeTrash(mutationsList, observer) {
+		// Get all attraction links with onclick attributes.
+		var trashLinks = document.querySelectorAll(".attraction_list [onclick]");
 
-			// Remove quotes from onclick attribute and separate into an array.
-			var clickData = el.getAttribute("onclick").replace(/'/g, "").split(",");
+		if (trashLinks) {
 
-			for (var x = 0; x < clickData.length; x++) {
-				var param = clickData[x].trim();
+			console.log("[TripAdvisor TRASH] Removing trash onclick elements.");
 
-				// Check if it's a URL.
-				if (/^\/.*?\.html$/.test(param)) {
+			for (var i = 0; i < trashLinks.length; i++) {
+				var el = trashLinks[i];
 
-					// Element is an anchor, so just add a href.
-					if (el.tagName === "A") {
-						el.setAttribute("href", param);
-						el.setAttribute("target", "_blank");
+				// Remove quotes from onclick attribute and separate into an array.
+				var clickData = el.getAttribute("onclick").replace(/'/g, "").split(",");
+
+				for (var x = 0; x < clickData.length; x++) {
+					var param = clickData[x].trim();
+
+					// Check if it's a URL.
+					if (/^\/.*?\.html$/.test(param)) {
+
+						// Element is an anchor, so just add a href.
+						if (el.tagName === "A") {
+							el.setAttribute("href", param);
+							el.setAttribute("target", "_blank");
+						}
+
+						// Not an anchor - wrap the inner HTML in an anchor and append to element.
+						else {
+							var link       = document.createElement("a");
+							link.href      = param;
+							link.target    = "_blank";
+							link.innerHTML = el.innerHTML;
+							el.innerHTML   = "";
+
+							el.append(link);
+						}
+
+						// Always remove the trash onclick attribute.
+						el.removeAttribute("onclick");
+
+						break;
 					}
-
-					// Not an anchor - wrap the inner HTML in an anchor and append to element.
-					else {
-						var link       = document.createElement("a");
-						link.href      = param;
-						link.target    = "_blank";
-						link.innerHTML = el.innerHTML;
-						el.innerHTML   = "";
-
-						el.append(link);
-					}
-
-					// Always remove the trash onclick attribute.
-					el.removeAttribute("onclick");
-
-					break;
 				}
 			}
+		} else {
+			console.log("[TripAdvisor TRASH] No trash onclick elements detected.");
 		}
-	} else {
-		console.log("[TripAdvisor TRASH] No trash onclick elements detected.");
 	}
 
 })();
