9ee45e16c5e56768d401a816409591eb3c69f9c9
Sapphire <33162278+sapphire-bt@users.noreply.github.com>
2019-04-20 09:54:22 +0100

    Update Increase Text Size.js

--- Increase Text Size.js
+++ Increase Text Size.js
@@ -1,35 +1,44 @@
 // ==UserScript==
 // @name         Increase Text Size
-// @version      2019-03-09
-// @description  Increase the font size of Arabic/Hebrew text on Wikipedia articles.
+// @version      2019-04-20
+// @description  Increase the font size of specified elements on Wikipedia.
 // @include      *wikipedia.org*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-	const elements = [
-		".mw-parser-output .script-hebrew",
-		".mw-parser-output .script-Hebr",
-		`span[lang="he"]`,
-		`span[lang="ar"]`
-	];
+    const elements = [
+        ".mw-parser-output .script-hebrew",
+        ".mw-parser-output .script-Hebr",
+        ".mw-parser-output .script-arabic",
+        ".mw-parser-output .script-Arab",
+        `[lang="he"]`,
+        `[lang="ar"]`,
+        `[lang="arz"]`,
+        `[lang="bs-Arab"]`,
+        `[lang="fa"]`,
+        `[lang="hi"]`,
+        `[lang="ps"]`,
+        `[lang="ur"]`,
+        `[lang="uz-Arab"]`
+    ];
 
-	const targetEls = document.querySelectorAll(elements.join(","));
-	const newSize   = 30;
+    const targetEls = document.querySelectorAll(elements.join(","));
+    const newSize   = 40;
 
-	for (let i = 0; i < targetEls.length; i++) {
-		let textEl   = targetEls[i];
-		let fontSize = parseFloat(window.getComputedStyle(textEl, null).getPropertyValue("font-size"));
+    for (let i = 0; i < targetEls.length; i++) {
+        const textEl   = targetEls[i];
+        const fontSize = parseFloat(window.getComputedStyle(textEl, null).getPropertyValue("font-size"));
 
-		if (fontSize < newSize) {
-			let currentStyle = textEl.getAttribute("style");
-			let inlineStyle  = `font-size: ${newSize}px !important;`;
+        if (fontSize < newSize) {
+            const currentStyle = textEl.getAttribute("style");
+            const inlineStyle  = `font-size: ${newSize}px !important;`;
 
-			if (currentStyle === null) {
-				textEl.setAttribute("style", inlineStyle);
-			} else {
-				textEl.setAttribute("style", `${currentStyle}; ${inlineStyle}`);
-			}
-		}
-	}
+            if (currentStyle === null) {
+                textEl.setAttribute("style", inlineStyle);
+            } else {
+                textEl.setAttribute("style", `${currentStyle}; ${inlineStyle}`);
+            }
+        }
+    }
 })();
