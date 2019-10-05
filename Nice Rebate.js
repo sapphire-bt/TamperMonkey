1f57a1142ac37ee0562407004d07b54774ec55b3
Sapphire <33162278+sapphire-bt@users.noreply.github.com>
2019-10-05 14:05:49 +0100

    Update Nice Rebate.js

--- Nice Rebate.js
+++ Nice Rebate.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Nice Rebate
-// @version      2019-10-03
+// @version      2019-10-05
 // @include      https://www.nicerebate.com/home/view_product/*
 // @include      https://www.nicerebate.com/home/orderList*
 // @grant        none
@@ -10,14 +10,24 @@ if (typeof jQuery === "function") {
     $(function() {
         $("[onclick^='openhref']").each(function() {
             const
-                onclickStr  = $(this).attr("onclick"),
-                amazonUrl   = "https://www." + onclickStr.substring(onclickStr.indexOf("amazon"), onclickStr.indexOf("?m=")),
-                buttonClass = document.location.href.indexOf("/view_product/") > -1 ? "btn btn-default btn-lg btn-block" : "btn btn-sm btn-info",
-                newButton   = $("<a>", {
-                    class  : buttonClass,
-                    href   : amazonUrl,
+                onclickStr = $(this).attr("onclick"),
+                newButton  = $("<a>", {
+                    class  : this.className,
                     target : "_blank"
                 }).text($(this).text());
+
+            let amazonUrl;
+
+            if (
+                   onclickStr.toLowerCase().indexOf("review")   > -1
+                || onclickStr.toLowerCase().indexOf("feedback") > -1
+            ) {
+                amazonUrl = onclickStr.split("'")[3];
+            } else {
+                amazonUrl = onclickStr.substring(onclickStr.indexOf("amazon"), onclickStr.indexOf("?m="));
+            }
+
+            newButton.attr("href", "https://www." + amazonUrl);
 
             $(this).parent().html(newButton);
         })
